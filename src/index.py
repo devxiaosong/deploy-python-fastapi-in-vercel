41a49ff85b77baa823b2d1e28b578bc624dfa52a
devxiaosong <164790906@qq.com>
2024-02-25 08:54:14 +0800

    测试对Device的引用

--- src/index.py
+++ src/index.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from pywidevine.cdm import Cdm
+from pywidevine.device import Device
+from pywidevine.pssh import PSSH
 from src.dtos.ISayHelloDto import ISayHelloDto
 
 app = FastAPI()
@@ -18,3 +21,15 @@ async def say_hello(name: str):
 @app.post("/hello")
 async def hello_message(dto: ISayHelloDto):
     return {"message": f"Hello {dto.message}"}
+
+
+@app.get("/cdm/device")
+async def cdm_device():
+    Device(
+        type_=Device.Types.ANDROID,
+        security_level=3,
+        flags={},
+        client_id='123',
+        private_key='456',
+    )
+    return {"message": "Device"}
